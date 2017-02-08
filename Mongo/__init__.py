7137c3b9dc55ace1a815a13b9831339325a012da
Leo Bernal <leobernal91@gmail.com>
2017-02-08 22:36:41 +0100

    MongoDB Connection

---
+++ Mongo/__init__.py
@@ -0,0 +1,2 @@
+# Lo usamos para poder cargar el Paquete Mongo en otra parte de nuestro programa
+# incluso estando vacio
---
+++ Mongo/connection.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+import pymongo
+from settings import MongoConfig
+
+
+class MongoDBconnection(object):
+
+    _nameDB = None
+
+    def __init__(self):
+        self._connectionDB()
+
+    def _connectionDB(self):
+        configuration = MongoConfig()
+        mongoClient = pymongo.MongoClient(host=configuration.host, port=configuration.port)
+        
+        # Sin autenticacion
+        db = mongoClient[configuration.db_name]
+
+        # Si mongo requiere autenticacion con usuario y contrasenna
+        if configuration.username is not None:
+            db.authenticate(configuration.username, password=configuration.password)
+
+        # Devolvemos la bbdd usada. En nuestro caso "rpCloud"
+        self._nameDB = db
+
+    def getDB(self):
+        return self._nameDB
