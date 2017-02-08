520d90ec252de3d848f878e9d06e23cde810a045
Leo Bernal <leobernal91@gmail.com>
2017-02-08 21:51:03 +0100

    Configuration

---
+++ Mongo/settings.py
@@ -0,0 +1,38 @@
+import pymongo
+
+
+config = {
+    # MONGO CONFIG
+    "USERNAME": None,
+    "PASSWORD": None,
+    "HOST": "localhost",
+    "PORT": 27017,
+    "DB_NAME": "rpCloud",
+}
+
+
+class Config(object):
+    def __init__(self):
+        self._config = config
+
+class MongoConfig(Config):
+
+    @property
+    def username(self):
+        return self._config['USERNAME']
+
+    @property
+    def password(self):
+        return self._config['PASSWORD']
+
+    @property
+    def host(self):
+        return self._config['HOST']
+
+    @property
+    def port(self):
+        return self._config['PORT']
+
+    @property
+    def db_name(self):
+        return self._config['DB_NAME']
